d485e9bf03c68198a622a510f277ad7b4adbaea2
bill <wbrouwer@slb.com>
2022-06-25 22:04:24 +0300

    ci: do not watch after running test, create xml report

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/app"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "cobertura" }],
     },
     reporters: ["spec"],
     port: 9876,
